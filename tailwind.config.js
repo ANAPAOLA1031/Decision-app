b0e62e91d2853bb2c2f14cff990876d7a0baa307
ANA <323030172@upjr.edu.mx>
2025-06-24 21:37:14 -0600

    App-Decidir

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx,vue}'],
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
